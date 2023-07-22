c4c6769c9971a53514ee1d36f6b9df77c1e01e3e
Alberto Simões <ambs@cpan.org>
2023-07-22 22:30:30 +0100

    abc in the way

---
+++ abc.lua
@@ -0,0 +1,35 @@
+-- Pandoc filter to process code blocks with class "abc" containing
+-- ABC notation into images.
+--
+-- * Assumes that abcm2ps and ImageMagick's convert are in the path.
+-- * For textual output formats, use --extract-media=abc-images
+-- * For HTML formats, you may alternatively use --embed-resources
+
+local filetypes = { html = {"png", "image/png"}
+                  , latex = {"pdf", "application/pdf"}
+                  }
+local filetype = filetypes[FORMAT][1] or "png"
+local mimetype = filetypes[FORMAT][2] or "image/png"
+
+local function abc2eps(abc, filetype)
+    local eps = pandoc.pipe("abcm2ps", {"-q", "-O", "-", "-"}, abc)
+    local final
+    if FORMAT == "latex" then
+        local pdf = pandoc.pipe("epstopdf", {"--filter"}, eps)
+        local crop_pdf = pandoc.pipe("pdfcrop", {"-", "__.pdf"}, pdf)
+        final = pandoc.pipe("cat", {"__.pdf"}, crop_pdf)
+        os.remove("__.pdf")
+    else
+        final = pandoc.pipe("convert", {"-trim", "-", filetype .. ":-"}, eps)
+    end
+    return final
+end
+
+function CodeBlock(block)
+    if block.classes[1] == "abc" then
+        local img = abc2eps(block.text, filetype)
+        local fname = pandoc.sha1(img) .. "." .. filetype
+        pandoc.mediabag.insert(fname, mimetype, img)
+        return pandoc.Para{ pandoc.Image({pandoc.Str("abc tune")}, fname) }
+    end
+end
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
 all.pdf: all.md
-	pandoc --filter	 tikz.py --top-level-division=chapter -t pdf -o all.pdf all.md
+	pandoc --filter	 tikz.py  --lua-filter=abc.lua --top-level-division=chapter -t pdf -o all.pdf all.md
 
-all.md: 0*.md fig*png
+all.md: 0*.md
 	./joiner.pl
 
 all.tex: all.md
-	pandoc --filter	 tikz.py --top-level-division=chapter -t latex -o all.tex all.md
+	pandoc --filter	 tikz.py --lua-filter=abc.lua --top-level-division=chapter -t latex -o all.tex all.md
