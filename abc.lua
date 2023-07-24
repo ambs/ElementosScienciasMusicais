cd73a47af79e73b6d483b5da74cb9674f90cf5ee
Alberto Simões <ambs@cpan.org>
2023-07-24 22:36:40 +0100

    Organizing things

--- abc.lua
+++ abc.lua
@@ -25,11 +25,24 @@ local function abc2eps(abc, filetype)
     return final
 end
 
+local function generateLaTeX(meta, filename)
+    -- caption = meta["caption"] or ""
+    -- if caption ~= "" then
+    --     caption = "\\caption{" .. caption .. "}\n"
+    -- end
+    local content = "\\begin{center}\n\\includegraphics{" .. filename .. "}\n\\end{center}"
+    return pandoc.RawBlock('latex', content)
+end
+
 function CodeBlock(block)
     if block.classes[1] == "abc" then
         local img = abc2eps(block.text, filetype)
         local fname = pandoc.sha1(img) .. "." .. filetype
         pandoc.mediabag.insert(fname, mimetype, img)
-        return pandoc.Para{ pandoc.Image({pandoc.Str("abc tune")}, fname) }
+        if FORMAT == "latex" then
+            return generateLaTeX(block.attributes, fname)
+        else
+            return pandoc.Para{ pandoc.Image({pandoc.Str("abc tune")}, fname) }
+        end
     end
 end
